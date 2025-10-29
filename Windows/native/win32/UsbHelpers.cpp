ee1d9c06c7c42ece230c6cccac47d0f81d07dc24
SSMG4 <matthew.gallon0912@gmail.com>
2025-10-29 13:34:22 +0100

    UsbHelpers.cpp

---
+++ Windows/native/win32/UsbHelpers.cpp
@@ -0,0 +1,11 @@
+#include "UsbHelpers.h"
+#include <windows.h>
+
+bool Usb_IsDeviceConnected() {
+    // Stub: always return false in the scaffold.
+    return false;
+}
+
+const char* Usb_GetLastError() {
+    return "UsbHelpers: no error (stub)";
+}
