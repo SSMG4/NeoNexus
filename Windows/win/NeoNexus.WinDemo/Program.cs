3b04c7096116c7726987e7e3ff34233ddedac806
SSMG4 <matthew.gallon0912@gmail.com>
2025-10-29 13:44:33 +0100

    Program.cs

---
+++ Windows/win/NeoNexus.WinDemo/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Windows.Forms;
+
+namespace NeoNexus.WinDemo
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
